8826b89817a2e526d645ed2fb71c50e51d10eb5c
Danil <shwedx@Yandex.ru>
2023-03-21 19:37:05 +0300

    2 task done

--- 2 task/Program.cs
+++ 2 task/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+
+int[] array = new int [9];
+for(int i=0;i<array.Length;i++)
+{
+    array[i] = new Random().Next(1,99);
+} 
+
+System.Console.WriteLine(string.Join(",",array));
+
+int getnumber(int[]array)
+{
+    int result = 0;
+    for(int i=1;i<array.Length;i=i+2)
+    {
+        result = result + array[i];
+    }
+
+
+
+    return result;
+}
+
+int result = getnumber(array);
+System.Console.WriteLine(result);
